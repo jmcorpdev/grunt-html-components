0abf839e7876c9e331488552162f5bb848dbd768
arnogues <arnaudgs@gmail.com>
2014-09-17 21:19:24 +0200

    finish the test for htmlcomponents and the task

--- test/html_components_test.js
+++ test/html_components_test.js
@@ -37,14 +37,16 @@ exports.html_components = {
         test.done();
     },*/
     custom_options: function (test) {
-        test.expect(1);
+        test.expect(5); //5 files expected
 
-        //var actual = grunt.file.read('tmp/custom_options');
-        //var expected = grunt.file.read('test/expected/custom_options');
-
-
-
-        test.equal(actual, expected, 'should describe what the custom option(s) behavior is.');
+        // loop on all file to check if they are equals
+        var files = grunt.file.expand('tmp/**/*.html');
+        files.forEach(function (filepath) {
+            var actual = grunt.file.read(filepath);
+            var expectedFile = filepath.replace('tmp/', 'test/expected/htmlpages/');
+            var expected = grunt.file.read(expectedFile);
+            test.equal(actual, expected, 'should generate a complete file : ' + expectedFile);
+        });
 
         test.done();
     }
